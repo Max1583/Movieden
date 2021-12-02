f09173737e650de66c3216d17adbb9d93ebb2385
Chibuike Nnaji <67190735+Max1583@users.noreply.github.com>
2021-12-02 08:58:57 +0100

    search overlay

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,55 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../Styles/Header.scss'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { FaPlayCircle } from 'react-icons/fa';
+import { FiSearch } from 'react-icons/fi';
+import { RiCloseLine } from 'react-icons/ri';
+import { HiMenuAlt4 } from 'react-icons/hi';
+
 
 function Header() {
-    return (
-        <header className="sticky-to">
 
-<nav className="navbar d-flex justify-content-center align-items-center">
-        <Link to="/" className="logo  text-center navbar-brand">
-            <span><FaPlayCircle/>MOVIE</span><span>DEN </span>
-        </Link>
-</nav>
-<nav className="sub-nav d-flex justify-content-start justify-content-md-center">
-<Link to="/" className="nav-link">Home</Link>
-<Link to="/trending" className="nav-link">Trending (Movies)</Link>
-<Link to="/trending-TvSeries" className="nav-link">Trending (TV Series)</Link>
-<Link to="/in-Theaters" className="nav-link">In Theaters</Link>
-</nav>
-            
+    const [isNav,setIsNav] = useState(false)
+const toggleNav=()=>{
+setIsNav(!isNav);
+}
+
+    return (
+        <header className="sticky-top">
+
+            <nav className="navbar d-flex justify-content-between container align-items-center ">
+                <Link to="/" className="logo  text-center navbar-brand fs-2">
+                    <span><FaPlayCircle />MOVIE</span><span>DEN </span>
+                </Link>
+
+
+                <div className="d-flex align-items-center" >
+
+                    <div className="search__icon fs-5 mx-3">
+                    < FiSearch  onClick={setIsNav(false)} />
+                    
+                </div>
+
+                <div className={isNav? "nav__links d-inline mobileNavActive":"nav__links d-inline "}>
+                    <Link onClick={toggleNav} to="/" className="nav-link">Home</Link>
+                    <Link onClick={toggleNav}  to="/trending" className="nav-link">Trending (Movies)</Link>
+                    <Link onClick={toggleNav} to="/trending-TvSeries" className="nav-link">Trending (TV Series)</Link>
+                    <Link onClick={toggleNav} to="/in-Theaters" className="nav-link">In Theaters</Link>
+                </div>
+
+                <div className="nav__toggle d-block d-lg-none fs-5" onClick={toggleNav}>
+                  {
+                     isNav ?< RiCloseLine className="nav__close"  /> :< HiMenuAlt4 />
+                  }  
+                    
+                </div>
+
+                </div>
+
+                
+            </nav>
+
+
         </header>
     )
 }
